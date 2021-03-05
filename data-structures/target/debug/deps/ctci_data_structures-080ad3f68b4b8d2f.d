7318930f8b85b3e12ef366297f7accc6d568894b
Derek Strickland <derekstrickland@pop-os.localdomain>
2021-03-05 09:50:25 -0500

    remove targets

--- data-structures/target/debug/deps/ctci_data_structures-080ad3f68b4b8d2f.d
+++
@@ -1,7 +0,0 @@
-/home/derekstrickland/Github/ctci-rs/data-structures/target/debug/deps/ctci_data_structures-080ad3f68b4b8d2f: src/lib.rs src/queue.rs src/stack.rs
-
-/home/derekstrickland/Github/ctci-rs/data-structures/target/debug/deps/ctci_data_structures-080ad3f68b4b8d2f.d: src/lib.rs src/queue.rs src/stack.rs
-
-src/lib.rs:
-src/queue.rs:
-src/stack.rs:
--- data-structures/target/debug/deps/data_structures-420df9ce7ce15df8.d
+++
@@ -1,5 +0,0 @@
-/home/derekstrickland/Github/ctci-rs/data-structures/target/debug/deps/data_structures-420df9ce7ce15df8: src/lib.rs
-
-/home/derekstrickland/Github/ctci-rs/data-structures/target/debug/deps/data_structures-420df9ce7ce15df8.d: src/lib.rs
-
-src/lib.rs:
